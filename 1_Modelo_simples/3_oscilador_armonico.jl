d4c3810082c6f1df07737c7cd9d6bb5aacc247e5
Miguel Ángel Riascos <73512305+dervalinor@users.noreply.github.com>
2024-04-24 00:16:39 +0000

    Algunas correcciones de variables

--- 1_Modelo_simples/3_oscilador_armonico.jl
+++ 1_Modelo_simples/3_oscilador_armonico.jl
@@ -1,13 +1,15 @@
 #crear un funcion para calcula los niveles de energia y las graficar niveles de energia
 #ademas de otras funciones mas
 using Plots
+using Calculus
 
 v = 0
 h = 6.62607015e-34
 E = 0
+v_f = 0
 
-function Energia_niveles(v)
-    return (v + 1/2) * h * v
+function Energia_niveles(v, v_f)
+    return (v + 1/2) * h * v_f
 end
 
 #Luego aplicar aproximaciones para obtener resultados para moleculas poliatomicas
